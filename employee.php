7f04c38f079c70ab25f4500ee9213529e4baa4d4
Anna Rose Limpangog <119985247+lunestelle@users.noreply.github.com>
2024-04-21 13:47:44 +0800

    FIX

--- employee.php
+++ employee.php
@@ -36,7 +36,7 @@ if (isset($_SESSION['s_em_email'])) {
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <!--Navbar CSS-->
-    <link rel="stylesheet" href="css/navbar2.css">
+    <link rel="stylesheet" href="css/navbar.css">
 
     <!--calendar links-->
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
