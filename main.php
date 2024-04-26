ec7990fb230bc7dbcce8afbd50e372d8a6809c45
Ghost <113078711+ghost-012@users.noreply.github.com>
2024-04-26 10:33:46 +0800

    inactive time update
    session inactive time updated to 2hours

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ session_start();
 include "DBConnection.php";
 
 // Check for user inactivity
-$inactive_timeout = 300; // 5 minutes in seconds
+$inactive_timeout = 7200; // 2 hours in seconds
 if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $inactive_timeout)) {
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
