1e0e326910046bbd8e1506b0853560700f3a3bc6
Anna Rose Limpangog <119985247+lunestelle@users.noreply.github.com>
2024-05-14 16:32:53 +0800

    Update Reports_employee.php

--- Reports_employee.php
+++ Reports_employee.php
@@ -58,7 +58,7 @@ if (!$result) {
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-12 p-4">
-      <div style="height:100vh;">
+      <div style="height:150vh;">
         <div class="d-flex justify-content-between align-items-center">
           <p class="fw-bold fs-5 text-uppercase">Employee Report</p>
         </div>
